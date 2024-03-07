0b5086a367abd972109c53526de0fd6a96206232
vincenzoaltavilla <vincenzoaltavilla.va@gmail.com>
2024-03-07 14:41:45 +0100

    checked the maximum number of steps

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ def sumSquaredDigits(n):
         sum = sum + (digit * digit)
     return sum
 
+NUMBERS = 1000000
+max = 0
 
-for n in range(1, 1001):
+for n in range(1, NUMBERS):
     n = str(n)
     sum = sumSquaredDigits(n)
     i = 0
 
     while sum != 89 and sum != 1:
         i = i + 1
+        if i > max:
+            max = i
+
         sum = sumSquaredDigits(str(sum))
 
     print(n, 'goes to', sum, 'through', i, 'steps')
+
+print('The maximum number of steps for the first', NUMBERS, 'numbers is', max)
